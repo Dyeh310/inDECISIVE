f2e07c31d93274161d38495e8e9da455dacb6e51
NMH <nheyd9292@gmail.com>
2020-12-31 11:17:36 -0500

    12/31

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,13 +1,17 @@
 import 'package:indecisive/models/game.dart';
 import 'package:indecisive/models/user.dart';
 
-var DUMMY_DATA_PLAYER = [
-  User(id: 'U1432423', userName: 'Nick', password: 'pass321'),
-  User(id: 'U1435431', userName: 'Anny', password: 'pass321'),
-  User(id: 'U3242312', userName: 'Chase', password: 'pass321',
-      // Games associated with that player
-      games: [
-        Game(name: 'Dying Light'),
-        Game(name: 'Vermintide', isCompleted: true)
-      ]),
-];
+List<User> users;
+
+List<User> get playerList {
+  return users = [
+    User(id: 'U1432423', userName: 'Nick', password: 'pass321'),
+    User(id: 'U1435431', userName: 'Anny', password: 'pass321'),
+    User(id: 'U3242312', userName: 'Chase', password: 'pass321',
+        // Games associated with that player
+        games: [
+          Game(name: 'Dying Light'),
+          Game(name: 'Vermintide', isCompleted: true)
+        ])
+  ];
+}
